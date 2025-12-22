8c8538c681f1454800e0f9e4bd764065651f3d39
angelkrodriguez <angel@bold.gt>
2025-12-22 10:51:43 -0600

    cambios

--- backend/src/application/dto/create-application.dto.ts
+++ backend/src/application/dto/create-application.dto.ts
@@ -1,34 +1,125 @@
 import { EstadoSolicitud } from '../entities/application.entity';
+import {
+  IsString,
+  IsOptional,
+  IsEmail,
+  IsBoolean,
+  IsArray,
+  ValidateNested,
+  IsEnum,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class PersonalDto {
+  @IsString()
+  firstName: string;
+
+  @IsOptional()
+  @IsString()
+  secondName?: string;
+
+  @IsOptional()
+  @IsString()
+  thirdName?: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  secondLastName?: string;
+
+  @IsOptional()
+  @IsString()
+  marriedLastName?: string;
+
+  @IsOptional()
+  @IsString()
+  birthDate?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  dpi?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  hasSixMonths?: boolean;
+}
+
+class UploadsDto {
+  @IsOptional()
+  dpi?: any;
+
+  @IsOptional()
+  bankStatements?: any;
+
+  @IsOptional()
+  electricityBill?: any;
+
+  @IsOptional()
+  selfieWithDpi?: any;
+}
+
+class ReferenceDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  phone: string;
+}
+
+class LegalDto {
+  @IsBoolean()
+  acceptance: boolean;
+
+  @IsBoolean()
+  consent: boolean;
+}
 
 export class CreateApplicationDto {
-  personal: {
-    firstName: string;
-    secondName?: string;
-    thirdName?: string;
-    lastName: string;
-    secondLastName?: string;
-    marriedLastName?: string;
-    birthDate?: string;
-    phone?: string;
-    dpi?: string;
-    email?: string;
-    hasSixMonths?: boolean;
-  };
+  @ValidateNested()
+  @Type(() => PersonalDto)
+  personal: PersonalDto;
 
-  uploads?: {
-    dpi?: any;
-    bankStatements?: any;
-    electricityBill?: any;
-    selfieWithDpi?: any;
-  };
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => UploadsDto)
+  uploads?: UploadsDto;
 
-  personalRefs?: { name: string; phone: string }[];
-  workRefs?: { name: string; phone: string }[];
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ReferenceDto)
+  personalRefs?: ReferenceDto[];
 
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ReferenceDto)
+  workRefs?: ReferenceDto[];
+
+  @IsOptional()
+  @IsString()
   salary?: string;
+
+  @IsOptional()
+  @IsString()
   source?: string;
 
-  legal?: { acceptance: boolean; consent: boolean };
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => LegalDto)
+  legal?: LegalDto;
 
+  @IsOptional()
+  @IsEnum(EstadoSolicitud)
   estadoSolicitud?: EstadoSolicitud;
 }
